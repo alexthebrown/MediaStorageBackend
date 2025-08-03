0e8e64093313a6b6f58d58c5534c0a6cba879ff2
alexthebrown <99758041+alexthebrown@users.noreply.github.com>
2025-08-03 00:19:21 -0500

    actor functions

--- controllers/mediaController.js
+++ controllers/mediaController.js
@@ -1,4 +1,5 @@
 const MediaItem = require('../models/mediaItem');
+const ActorItem = require('../models/actorItem'); 
 
 exports.getAllMedia = async (req, res) => {
   const items = await MediaItem.find();
@@ -20,3 +21,33 @@ exports.deleteMediaItem = async (req, res) => {
   await MediaItem.findByIdAndDelete(req.params.id);
   res.status(204).end();
 };
+
+exports.addActor = async (req, res) => {
+    const { actors } = req.body; // array of ObjectId strings
+    const mediaId = req.params.id;
+  
+    try {
+      // Step 1: Add actors to the media item
+      const mediaItem = await MediaItem.findByIdAndUpdate(
+        mediaId,
+        { $addToSet: { actors: { $each: actors } } },
+        { new: true }
+      );
+  
+      if (!mediaItem) {
+        return res.status(404).json({ error: 'Media item not found' });
+      }
+  
+      // Step 2: Add media item to each actor's filmography
+      await ActorItem.updateMany(
+        { _id: { $in: actors } },
+        { $addToSet: { filmography: mediaId } }
+      );
+  
+      res.json(mediaItem);
+    } catch (err) {
+      console.error('Error in addActor:', err);
+      res.status(500).json({ error: 'Failed to update media and actor records' });
+    }
+  };
+  
